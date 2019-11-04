94e2a37584575a64f288c85e4b219499b049b711
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2019-11-04 09:54:32 -0500

    updated

--- GeoLocator.py
+++ GeoLocator.py
@@ -1,10 +1,11 @@
-__version__ = '0.1'
-__author__ = 'Kalle Westerling'
-
 
 #### Settings #################
 
-LOCATION_CACHE_DIRECTORY = '/Users/kallewesterling/_twitter_cache/locations/'
+LOCATION_CACHE_DIRECTORY = '/Users/kallewesterling/_twitter_cache/geolocated-locations/'
+STRICT = False       # New feature: If True, it will crash when it tries to create empty Location objects. If False, allows for empty Location objects.
+
+# Not yet implemented (but should be):
+BLOCK_LOCATIONS = ['a', 'list', 'of', 'locations', 'in', 'dataset', 'that', 'will', 'be', 'ignored']
 
 ###############################
 
@@ -12,13 +13,24 @@ LOCATION_CACHE_DIRECTORY = '/Users/kallewesterling/_twitter_cache/locations/'
 import time, json, re
 from random import randint
 from pathlib import Path
+
+# dependency
 from geopy import Nominatim
+
+# strange dependencies... my own package; needs to be downloaded from GitHub
 import CleanText
+
+
+
+
+
+print(f"Running GeoLocator in location {Path(__file__).parent}")
 
 
 class LocationCache():
 
     def __init__(self, location):
+        self.error = False
         self.location = location
         self.cache_directory = LocationCacheDirectory()
         self.cache_file = self.cache_directory.path / Path(location + ".json")
@@ -44,47 +56,67 @@ class LocationCache():
 
 class Location():
 
-    def __init__(self, location):
+    def __init__(self, location = None, block = []):
         cleaner = CleanText.CleanText(location)
         cleaner.digits = False
         cleaner.clean()
 
         self.log = Log()
+        self.error = False
 
         self.clean_location = cleaner.text
+        
+        if len(block):
+            self.clean_location = self.clean_location.split()
+            self.clean_location = ' '.join([i for i in self.clean_location if i not in block])
 
-        self.cache = LocationCache(self.clean_location)
+        if len(self.clean_location):
+          self.cache = LocationCache(self.clean_location)
 
-        if not self.cache.data:
-            self.data = self.get_data()
-            if self.data is not None:
+          if not self.cache.error:
+            if not self.cache.data:
+                self.data = self.get_data()
+                if self.data is None:
+                    self.data = {'error': 'not found'}
+                    self.log.log(f"Could not find geographic information for location {self.clean_location}")
                 self.cache._dump(self.data)
             else:
-                self.log.log(f"Could not find geographic information for location {self.clean_location}")
+                self.data = self.cache.data
+
+            if self.data is not None:
+              self.lat = self.data.get("lat", None)
+              self.lng = self.data.get("lng", None)
+              self.geolocator = self.data.get("geolocator", None)
+              self._type = self.data.get("type", None)
+              self._class = self.data.get("class", None)
+              self.display_name = self.data.get("display_name", None)
+              self.boundingbox = self.data.get("boundingbox", None)
+            else:
+              if STRICT:
+                raise RuntimeError(f"Data is none in location {location}")
+              else:
+                self.lat, self.log, self.geolocator, self._type, self._class, self.display_name, self.boundingbox = None, None, None, None, None, None, None
+                self.error = True
         else:
-            self.data = self.cache.data
-
-        self.lat = self.data.get("lat", None)
-        self.lng = self.data.get("lng", None)
-        self.geolocator = self.data.get("geolocator", None)
-        self._type = self.data.get("type", None)
-        self._class = self.data.get("class", None)
-        self.display_name = self.data.get("display_name", None)
-        self.boundingbox = self.data.get("boundingbox", None)
-
+          if STRICT:
+            raise RuntimeError("Location needs to not be empty.")
+          else:
+            self.lat, self.log, self.geolocator, self._type, self._class, self.display_name, self.boundingbox, self.error = None, None, None, None, None, None, None, True
+            self.data = {'error': 'location empty'}
 
     def get_data(self, geolocator="Nominatim"):
         if geolocator == "Nominatim":
             self.log.log("Geolocator chosen: Nominatim.")
-            sleep_val = randint(2,5)
-            self.log.log(f"Sleeping for {sleep_val} seconds...")
-            time.sleep(sleep_val)
             _geolocator = Nominatim(user_agent="tags-research-app")
         else:
             raise NotImplementedError("You can currently only use Nomatim as geolocator in this script.")
 
         try:
             geocoded_location = _geolocator.geocode(self.clean_location)
+
+            sleep_val = randint(2,5)
+            self.log.log(f"Sleeping for {sleep_val} seconds...")
+            time.sleep(sleep_val)
 
             if geocoded_location is not None:
                 data = {
